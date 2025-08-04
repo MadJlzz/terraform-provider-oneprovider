c3494b981da5407c52bcf9f3368c3feaa8cefcf4
Julien Klaer <j.klaer@tadaweb.com>
2025-08-04 18:44:51 +0200

    fix(svc): removing ssh service for now

--- pkg/oneprovider/service.go
+++ pkg/oneprovider/service.go
@@ -2,13 +2,12 @@ package oneprovider
 
 import (
 	"github.com/MadJlzz/terraform-provider-oneprovider/pkg/oneprovider/client"
-	"github.com/MadJlzz/terraform-provider-oneprovider/pkg/oneprovider/ssh"
 	"github.com/MadJlzz/terraform-provider-oneprovider/pkg/oneprovider/vm"
 )
 
 type Service struct {
-	VM  *vm.Service
-	SSH *ssh.Service
+	VM *vm.Service
+	//SSH *ssh.Service
 }
 
 func NewService(endpoint, apiKey, clientKey string) (*Service, error) {
@@ -17,7 +16,7 @@ func NewService(endpoint, apiKey, clientKey string) (*Service, error) {
 		return nil, err
 	}
 	return &Service{
-		VM:  vm.NewService(c),
-		SSH: ssh.NewService(c),
+		VM: vm.NewService(c),
+		//SSH: ssh.NewService(c),
 	}, nil
 }
